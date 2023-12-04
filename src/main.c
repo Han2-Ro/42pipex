139a47f5906ed79b501d8c22da9f353456bce4a8
Hannes Erik Rother <hrother@c2r7p5.42vienna.com>
2023-12-04 19:25:16 +0100

    fixed leaks for error cases

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: hrother <hrother@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 23:28:44 by hannes            #+#    #+#             */
-/*   Updated: 2023/12/04 19:04:17 by hrother          ###   ########.fr       */
+/*   Updated: 2023/12/04 19:24:22 by hrother          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,35 +36,6 @@ void	print_cmd(t_command cmd)
 	ft_printf("\n");
 }
 
-int    exec_prgr(char *path, char *args[], char *env[], int in, int out, int close_fd)
-{
-	int pid;
-
-	pid = fork();
-	if(pid < 0)
-	{
-		perror("fork");
-		exit(2);
-	}
-	if (pid == 0)
-	{
-		dup2(in, STDIN_FILENO);
-		close(in);
-		dup2(out, STDOUT_FILENO);
-		close(out);
-		close(close_fd);
-		if (access(path, X_OK) == 0)
-			execve(path, args, env);
-		else
-		{
-			perror("access");
-			exit(3);
-		}
-		ft_printf("This shouldn't be printed\n");
-	}
-	return (pid);
-}
-
 void free_strs(char **arr)
 {
 	int i;
@@ -84,10 +55,38 @@ void	free_cmd(t_command cmd)
 	free_strs(cmd.args2);
 }
 
+void exit_onerror(char *msg, t_command cmd)
+{
+	free_cmd(cmd);
+	perror(msg);
+	exit(1);
+}
+
+int    exec_prgr(char *path, char *args[], char *env[], int in, int out, int close_fd)
+{
+	int pid;
+
+	pid = fork();
+	if(pid < 0)
+		return(-1);
+	if (pid == 0)
+	{
+		dup2(in, STDIN_FILENO);
+		close(in);
+		dup2(out, STDOUT_FILENO);
+		close(out);
+		close(close_fd);
+		if (access(path, X_OK) == 0)
+			execve(path, args, env);
+		else
+			return (-1);
+		ft_printf("This shouldn't be printed\n");
+	}
+	return (pid);
+}
+
 int main(int argc, char **argv)
 {
-	//char *args1[] = {"/bin/ls", "-l", "-a", NULL};
-	//char *args2[] = {"/bin/grep","d", NULL};
 	int pipe_fd[2];
 	int pid1;
 	int pid2;
@@ -102,19 +101,17 @@ int main(int argc, char **argv)
 		return (2);
 	file1 = open(cmd.file1, O_RDONLY);
 	if (file1 < 0)
-	{
-		perror("file1");
-		exit(1);
-	}
+		exit_onerror("file1", cmd);
 	pid1 = exec_prgr(cmd.cmd1, cmd.args1, NULL, file1,  pipe_fd[1], pipe_fd[0]);
+	if (pid1 < 0)
+		exit_onerror("exec", cmd);
 	close(file1);
 	file2 = open(cmd.file2, O_RDWR | O_CREAT | O_TRUNC, 0644);
 	if (file2 < 0)
-	{
-		perror("file2");
-		exit(1);
-	}
+		exit_onerror("file2", cmd);
 	pid2 = exec_prgr(cmd.cmd2, cmd.args2, NULL, pipe_fd[0], file2, pipe_fd[1]);
+	if (pid1 < 0)
+		exit_onerror("exec", cmd);
 	close(file2);
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
